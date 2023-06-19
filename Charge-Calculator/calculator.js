0f25b6ff0486ed444302d8bf9599f11002ab7bc7
Snapzen <christopher_asen@hotmail.com>
2023-06-19 02:28:56 +0200

    New update for live

--- Charge-Calculator/calculator.js
+++ Charge-Calculator/calculator.js
@@ -1,62 +1,41 @@
 const chargeList = [
     { charge: '', fine: 0, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 0, confiscate: 0 },
-    { charge: 'Speeding - 1st Offense', fine: 0, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 0, confiscate: 0 },
-    { charge: 'Speeding - 2nd Offense', fine: 15, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 15, confiscate: 0 },
-    { charge: 'Speeding - Continuous', fine: 50, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 50, confiscate: 0 }, 
-    { charge: 'Loitering - 1st Offense', fine: 0, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 0, confiscate: 0 },
-    { charge: 'Loitering - 2nd Offense', fine: 15, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 15, confiscate: 0 }, 
-    { charge: 'Loitering - Continuous', fine: 50, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 15, confiscate: 0 },
-    { charge: 'Failure To Maintain Control Of Any Large Animal - 1st Offense', fine: 0, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 0, confiscate: 0 }, 
-    { charge: 'Failure To Maintain Control Of Any Large Animal - 2nd Offense', fine: 15, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 15, confiscate: 0 },
-    { charge: 'Failure To Maintain Control Of Any Large Animal - Continuous', fine: 50, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 50, confiscate: 0 }, 
-    { charge: 'Animal Cruelty - 1st Offense', fine: 30, jail: 10, note: true, stackable: false, stackJailCap: 10, stackFineCap: 30, confiscate: 0 },
-    { charge: 'Animal Cruelty - Continuous', fine: 50, jail: 10, note: true, stackable: false, stackJailCap: 10, stackFineCap: 50, confiscate: 0 }, 
-    { charge: 'Trespassing - 1st Offense', fine: 20, jail: 15, note: true, stackable: false, stackJailCap: 15, stackFineCap: 20, confiscate: 0 },
-    { charge: 'Trespassing - 2nd Offense', fine: 50, jail: 15, note: true, stackable: false, stackJailCap: 15, stackFineCap: 50, confiscate: 0 }, 
-    { charge: 'Trespassing - Continuous', fine: 80, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 80, confiscate: 0 },
-    { charge: 'Criminal Damage - 1st Offense', fine: 15, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 15, confiscate: 0 }, 
-    { charge: 'Criminal Damage - 2nd Offense', fine: 25, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 25, confiscate: 0 },
-    { charge: 'Criminal Damage - Continuous', fine: 30, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 30, confiscate: 0 }, 
-    { charge: 'Fraud', fine: 100, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 100, confiscate: 4 },
-    { charge: 'Criminal Threats', fine: 25, jail: 5, note: true, stackable: true, stackJailCap: 5, stackFineCap: 75, confiscate: 0 }, 
-    { charge: 'Disorderly Conduct - 1st Offense', fine: 25, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 25, confiscate: 0 },
-    { charge: 'Disorderly Conduct - 2nd Offense', fine: 50, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 50, confiscate: 0 }, 
-    { charge: 'Disorderly Conduct - Continuous', fine: 80, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 80, confiscate: 0 },
-    { charge: 'Possession of a Scoped Weapon (No License)', fine: 100, jail: 0, note: false, stackable: false, stackJailCap: 0, stackFineCap: 100, confiscate: 1 },
+    { charge: 'Speeding', fine: 150, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 150, confiscate: 0 },
+    { charge: 'Loitering', fine: 250, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 250, confiscate: 0 },
+    { charge: 'Animal Cruelty', fine: 200, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 200, confiscate: 0 },
+    { charge: 'Trespassing', fine: 200, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 200, confiscate: 0 },
+    { charge: 'Criminal Threats', fine: 250, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 250, confiscate: 0 }, 
+    { charge: 'Disorderly Conduct', fine: 150, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 150, confiscate: 0 },
+    { charge: 'Crimes against a Local', fine: 250, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 250, confiscate: 0 },
 
-    { charge: 'Theft - Item(s) Worth < $150', fine: 25, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 25, confiscate: 3 }, 
-    { charge: 'Theft - Item(s) Worth > $150', fine: 50, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 50, confiscate: 3 },
-    { charge: 'Theft - Item(s) Worth > $500', fine: 100, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 100, confiscate: 3 }, 
-    { charge: 'Theft - Item(s) Worth > $1500', fine: 150, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 150, confiscate: 3 },
-    { charge: 'Corruption Of A Court Officer', fine: 30, jail: 15, note: false, stackable: true, stackJailCap: 15, stackFineCap: 90, confiscate: 0 }, 
-    { charge: 'Impersonating A Court Officer', fine: 250, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 250, confiscate: 3 }, 
-    { charge: 'Brandishing', fine: 25, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 },
-    { charge: 'Assault', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 }, 
-    { charge: 'Assault - 1st Offense', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 },
-    { charge: 'Assault - 2nd Offense', fine: 35, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 35, confiscate: 0 }, 
-    { charge: 'Assault - Continuous', fine: 50, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 50, confiscate: 0 },
-    { charge: 'Kidnapping', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 }, 
-    { charge: 'Obstruction Of A Court Officer', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 }, 
-    { charge: 'Evading Justice', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 },
-    { charge: 'Failure To Comply With A Lawful Order', fine: 25, jail: 5, note: false, stackable: false, stackJailCap: 5, stackFineCap: 25, confiscate: 0 }, 
-    { charge: 'Assisting A Fugitive From Justice', fine: 50, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 50, confiscate: 0 },
+    { charge: 'Corruption Of A Court Officer', fine: 450, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 450, confiscate: 0 }, 
+    { charge: 'Brandishing', fine: 300, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 300, confiscate: 0 },
+    { charge: 'Unlawful Discharge of a Weapon', fine: 350, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 350, confiscate: 1 },
+    { charge: 'Concealment of Identity', fine: 400, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 400, confiscate: 0 },
+    { charge: 'Assault', fine: 400, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 400, confiscate: 0 }, 
+    { charge: 'Obstruction Of A Court Officer', fine: 400, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 400, confiscate: 0 },
+    { charge: 'Illegal Duelling', fine: 450, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 450, confiscate: 1 }, 
+    { charge: 'Evading Justice', fine: 300, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 300, confiscate: 0 },
+    { charge: 'Failure To Comply With A Lawful Order', fine: 350, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 350, confiscate: 0 }, 
+    { charge: 'Assisting A Fugitive From Justice', fine: 450, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 450, confiscate: 0 },
 
-    { charge: 'Dueling', fine: 50, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 50, confiscate: 1 }, 
-    { charge: 'Arson', fine: 75, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 75, confiscate: 2 },
-    { charge: 'Unlawful Discharge Of A Weapon', fine: 30, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 30, confiscate: 1 }, 
-    { charge: 'Class A Contraband', fine: 15, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 },
-    { charge: 'Class B Contraband', fine: 35, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 }, 
+    { charge: 'Kidnapping', fine: 500, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 500, confiscate: 0 },
+    { charge: 'Impersonating a Court Officer', fine: 500, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 500, confiscate: 6 }, 
+    { charge: 'Arson', fine: 600, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 600, confiscate: 2 },
+    { charge: 'Attempted Robbery', fine: 800, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 800, confiscate: 5 }, 
+    { charge: 'Robbery of Stores, Wagons & Oilrig', fine: 850, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 850, confiscate: 5 },
+    { charge: 'Robbery of Forts & Banks', fine: 1000, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 1000, confiscate: 5 },
+    { charge: 'Attempted Murder', fine: 1000, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 1000, confiscate: 1 }, 
+    { charge: 'Murder', fine: 1300, jail: 30, note: false, stackable: false, stackJailCap: 30, stackFineCap: 1300, confiscate: 1 },
+    { charge: 'Possession of Dynamite', fine: 1500, jail: 30, note: false, stackable: true, stackJailCap: 30, stackFineCap: 1500, confiscate: 2 }, 
+    { charge: 'Attempted Murder of a Lawman', fine: 1250, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 1250, confiscate: 1 }, 
+    { charge: 'Murder of a Lawman', fine: 1500, jail: 30, note: false, stackable: false, stackJailCap: 30, stackFineCap: 1500, confiscate: 1 }, 
+    { charge: 'Use of a Marksmen in Major Crimes', fine: 700, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 700, confiscate: 7 },
+
+    { charge: 'Class A Contraband', fine: 1000, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 },
+    { charge: 'Class B Contraband', fine: 150, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 }, 
     { charge: 'Class C Contraband', fine: 100, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 },
-    { charge: 'Attempted Robbery', fine: 100, jail: 10, note: false, stackable: false, stackJailCap: 10, stackFineCap: 100, confiscate: 5 }, 
-    { charge: 'Robbery', fine: 200, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 200, confiscate: 5 },
-    { charge: 'Attempted Murder', fine: 100, jail: 15, note: false, stackable: false, stackJailCap: 15, stackFineCap: 100, confiscate: 1 }, 
-    { charge: 'Attempted Mass Murder', fine: 200, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 200, confiscate: 1 },
-    { charge: 'Murder', fine: 200, jail: 20, note: false, stackable: false, stackJailCap: 20, stackFineCap: 200, confiscate: 1 }, 
-    { charge: 'Mass Murder', fine: 250, jail: 25, note: false, stackable: false, stackJailCap: 25, stackFineCap: 250, confiscate: 1 },
-    { charge: 'Violent Incompliance Withing Custody', fine: 50, jail: 10, note: true, stackable: true, stackJailCap: 10, stackFineCap: 150, confiscate: 0 }, 
-    { charge: 'Attempted Murder of a Government Official', fine: 50, jail: 15, note: true, stackable: true, stackJailCap: 15, stackFineCap: 9999999, confiscate: 1 }, 
-    { charge: 'Murder of a Government Official', fine: 100, jail: 30, note: true, stackable: true, stackJailCap: 30, stackFineCap: 9999999, confiscate: 1 }, 
-    { charge: 'Use of a Marksmen in Major Crimes', fine: 50, jail: 5, note: true, stackable: false, stackJailCap: 5, stackFineCap: 50, confiscate: 1 }
+    { charge: 'Class D Contraband', fine: 50, jail: 0, note: false, stackable: true, stackJailCap: 0, stackFineCap: 9999999, confiscate: 2 }
 ];
 
 const confiscates = [
@@ -65,7 +44,9 @@ const confiscates = [
     { id: 2, conf: "Contraband & Potential Cash (From Contraband Sales)" },
     { id: 3, conf: "Amount of appraisal of possessions taken from person" },
     { id: 4, conf: "Proven extorted Goods, Weapons, or Money" },
-    { id: 5, conf: "Removal of all Weapons and Contraband. If surrender only removal of Contraband" }
+    { id: 5, conf: "Removal of all Weapons and Contraband. If surrender only removal of Contraband" },
+    { id: 6, conf: "Any documents used to falsely pose as a government official" },
+    { id: 7, conf: "Scoped Weapon" }
 ];
 
 $(document).ready(function(){
@@ -247,19 +228,19 @@ function updateTotalFine(){
     if (totalFine > 500) {
         if($('#harsh-mode').is(':checked')){
             if($('#reduce-fine').is(':checked')){
-                totalFine = totalFine - (totalFine * .25);
+                totalFine = totalFine - (totalFine * .5);
             };
             totalFineBox.val(totalFine);
         } else {
             if($('#reduce-fine').is(':checked')){
-                totalFineBox.val(500 - (500 * .25));
+                totalFineBox.val(500 - (500 * .5));
             } else {
                 totalFineBox.val(500);
             }
         }
     } else {
         if($('#reduce-fine').is(':checked')){
-            totalFine = totalFine - (totalFine * .25);
+            totalFine = totalFine - (totalFine * .5);
         };
         totalFineBox.val(totalFine);
     } 
@@ -312,7 +293,7 @@ function resetCalc(){
     $('#totalJail').val(0);
     $('#totalFine').val(0);
     $('#confiscate').val("N/A");
-    $('#harsh-mode').prop('checked', false);
+    $('#harsh-mode').prop('checked', true);
     $('#reduce-fine').prop('checked', false);
 }
 
